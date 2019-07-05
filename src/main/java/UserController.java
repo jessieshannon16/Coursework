568d5135603688fcdeb6fa8835a11409c9c0c1a0
88460 <88460@farnborough.ac.uk>
2019-07-05 11:33:25 +0100

    Done some more stuff i guess

--- src/main/java/UserController.java
+++ src/main/java/UserController.java
@@ -1,3 +1,15 @@
-public class UserController {
-    public static void insertUser ()
+public class StudentsController {
+    public static void selectStudent (){
+        try{
+            PreparedStatement ps = db.prepareStatement("SELECT StudentName, StudentUsername, Password, AdultUsername, Level FROM Students");
+
+            ResultSet results = ps.executeQuery();
+            while (results.next()){
+                String StudentName = results.getString(1);
+                String StudentUsername = results.getString(2);
+                String password = results.getString(3);
+                String AdultUsername = results.getString(4);
+            }
+        }
+    }
 }
