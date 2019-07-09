41a79ccae1dbefbb89ae1f41825a017fe4b8e831
jessieshannon16 <88460@farnborough.ac.uk>
2019-07-09 10:24:56 +0100

    Merge branch 'master' of https://github.com/jessieshannon16/Coursework
    # Conflicts:
    #	.idea/misc.xml
    #	.idea/modules.xml
    #	.idea/vcs.xml
    #	Coursework.iml

--- src/main/java/StudentsController.java
+++ src/main/java/StudentsController.java
@@ -6,7 +6,7 @@ public class StudentsController {
     public static void selectStudent (){
         try{
             PreparedStatement ps = Main.db.prepareStatement("SELECT StudentName, StudentUsername, Password, AdultUsername, Level FROM Students");
-
+            // test pull works
             ResultSet results = ps.executeQuery();
             while (results.next()){
                 String StudentName = results.getString(1);
@@ -16,6 +16,13 @@ public class StudentsController {
             }
         } catch (SQLException e) {
             e.printStackTrace();
+        }
+    }
+    public static void insertStudent(String StudentName, String StudentUsername, String password, String AdultUsername){
+        try{
+            PreparedStatement ps = Main.db.prepareStatement( "INSERT INTO StudentS (StudentName, StudentUsername, password, AdultUsername) VALUES (?,?,?,?)");
+        }catch (SQLException e){
+            e.printStackTrace();}
         }
     }
 }
